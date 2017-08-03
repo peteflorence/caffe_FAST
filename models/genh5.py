1ea6c9eaadafcdb962196ec75e8437d91b7eb01a
peteflorence <pete.florence@gmail.com>
2017-08-03 18:50:28 -0400

    @travelbureau -- see, python always > matlab

--- models/genh5.py
+++ models/genh5.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import h5py
 import os
 import scipy.misc
+import skimage
 
 print("generating hdf5 data")
 
@@ -19,8 +20,8 @@ X = scipy.misc.imread('00177.png')
 Y = scipy.misc.imread('00177_graybel.png')
 
 with h5py.File(h5_fn, 'w') as f:
-   f['data'] = X
-   f['label'] = Y
+   f['data'] = skimage.img_as_float(X)
+   f['label'] = skimage.img_as_float(Y)
 
 text_fn = os.path.join(DIR, dataset_name+'.txt')
 with open(text_fn, 'w') as f:
